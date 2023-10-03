12e248da10d406b016eab0a72ea75c66be808fca
Narayan Jha <jha_narayanji@yahoo.com>
2023-10-03 01:45:40 +0000

    basic update

---
+++ 01_basic/01_basic.js
@@ -0,0 +1,12 @@
+const accountId = 23232;
+let accountEmail = "narayan@gmail.com";
+var accountPassword = "123456"
+accountCity = "Ghaziabad"
+let accountState;
+//accountId = 3 // not allowed
+/*
+Prefer not use of var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
