a1ab0d3af98d24e769baacff17074318a776ccf1
Narayan Jha <jha_narayanji@yahoo.com>
2023-10-03 02:16:05 +0000

    dataTypes update

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3) // we are useing nodejs, not browser
+console.log(4+
+    4); // code readability should be high
+
+let name = "Narayan";
+let age = 22;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53 (store in memory)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unidefined
+
+// object =>
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
